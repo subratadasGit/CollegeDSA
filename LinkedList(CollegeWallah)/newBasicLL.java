81f2cb1043063230146c947e2db6385d83a40807
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-03-18 13:15:38 +0000

    For loop

---
+++ LinkedList(CollegeWallah)/newBasicLL.java
@@ -0,0 +1,43 @@
+public class newBasicLL {
+
+    static class Node{
+        int data;
+        Node Next;
+    
+        Node(int data){
+            this.data = data;
+        }
+    }
+        public static void main(String[] args) {
+    
+            Node a = new Node(0);
+            Node b = new Node(2);
+            Node c = new Node(3);
+            Node d = new Node(4);
+            Node e = new Node(5);
+            
+    
+            a.Next=b;
+            b.Next=c;
+            c.Next=d;
+            d.Next=e;
+
+            Node temp = a;
+
+           for(int i = 1 ; i<= 5 ; i++){
+
+
+            System.out.println(temp.data);
+            temp = temp.Next;
+
+
+
+           }
+
+            
+    
+            
+    
+    
+        }
+    }
