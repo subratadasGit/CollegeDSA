3cde4f10a329a73eba13c39f6a67a75dac433672
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-03-18 11:05:35 +0000

    LinkedList1

---
+++ LinkedList(CollegeWallah)/basicLl.java
@@ -0,0 +1,35 @@
+public class basicLl {
+
+static class Node{
+    int data;
+    Node Next;
+
+    Node(int data){
+        this.data = data;
+    }
+}
+    public static void main(String[] args) {
+
+        Node a = new Node(0);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        
+
+        a.Next=b;
+        b.Next=c;
+        c.Next=d;
+        d.Next=e;
+
+
+        System.out.println(a.data);
+        System.out.println(a.Next.data);
+
+        
+
+        
+
+
+    }
+}
